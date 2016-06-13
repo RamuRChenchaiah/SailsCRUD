5576398181e08749b5359bdd39c9f154c7cdbab9
RC Ramu <rchencha@cisco.com>
2016-06-13 06:29:14 +0530

    Updating Employee Model.

--- api/models/Employee.js
+++ api/models/Employee.js
@@ -1,14 +1,17 @@
 /**
  * Employee.js
  *
- * @description :: TODO: You might write a short summary of how this model works and what it represents here.
- * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
- */
+*/
 
 module.exports = {
 
   attributes: {
-
+  		name: 'STRING',
+  		ACN : 'STRING',
+  		address: 'STRING',
+  		department: 'STRING',
+  		group: 'STRING'  		
   }
 };
+
 
